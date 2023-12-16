c1e3f261c504b1be600dfba341ea07dc018dccd1
iam-Aman0506 <andraskarabhiram46@gmail.com>
2023-12-16 21:02:25 +0530

    CAT

---
+++ light_on_off/main.js
@@ -0,0 +1,10 @@
+function btn_on() {
+    document.getElementById('bulb').src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png';
+    document.getElementById('cat').src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png';
+}
+function btn_off() {
+    document.getElementById('bulb').src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png';
+    document.getElementById('cat').src='https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png';
+}
+
+
